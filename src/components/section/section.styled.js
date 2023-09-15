1b08fc5dee6469bc07c2f94ef72747422b0305af
anastasiiahmel <126502776+anastasiiahmel@users.noreply.github.com>
2023-09-15 13:22:56 +0300

    HW_2

---
+++ src/components/section/section.styled.js
@@ -0,0 +1,12 @@
+import { styled } from 'styled-components';
+
+export const SectionStyle = styled.div`
+  /* display: flex; */
+  .title {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Times New Roman', Times, serif;
+    font-size: 20px;
+  }
+`;
